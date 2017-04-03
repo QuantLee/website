62f576a93b291485f90b0147c4b45cad3648e712
QuantLee <leodongxu@gmail.com>
2017-04-03 00:40:36 -0400

    change the size of the line

--- sketch.js
+++ sketch.js
@@ -1,8 +1,9 @@
 var w = 1400;
 var h = 800;
-var drift = 0.0001
+var drift = 0.001
 var variance = 0.05;
-var timeline = 2;
+var timeline = 5;
+var limit = 50
 
 var dot = {
   x: 0,
@@ -21,14 +22,20 @@ function setup(){
 }
 
 function draw(){
-  //background(0);
   stroke(col.r,col.g,col.b);
   x = dot.x;
   y = dot.y;
+  if (dot.y > h-limit){
+    dot.y = dot.y - 50;
+  }
+  if (dot.y < 50){
+    dot.y = dot.y + 50;
+  }
   dot.x = dot.x + timeline;
   dot.y = dot.y * exp(-drift +random(-variance,variance));
   strokeWeight(2);
   line(x,y,dot.x,dot.y);
+
 }
 
 
